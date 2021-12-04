02ea041ff6278d67744879621dd651a0f7f19d28
Ahmed khan <ahmed_khan019@hotmail.com>
2021-12-04 21:21:44 +0500

    updated config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   target: 'static',
   router: {
-    base: 'nuxt-dadjokes-app'
+    base: '/nuxt-dadjokes-app'
   },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
